cf5657238592b6b7d006849d17ca250b008f464e
haikali3 <manfdvcl9@gmail.com>
2024-11-24 21:34:55 +0800

    Add Post model, migration, and seed data; integrate Chartkick for enhanced crypto dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_24_004440) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_24_064539) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -35,6 +35,11 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_24_004440) do
     t.decimal "price"
     t.decimal "market_cap"
     t.decimal "volume"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+(1..100).each do |i|
+  Post.find_or_create_by!(title: "Post #{i}") do |post|
+    post.created_at = Time.now - i.days
+    post.updated_at = Time.now - i.days
+    post.views = rand(15..100)
+  end
+end
